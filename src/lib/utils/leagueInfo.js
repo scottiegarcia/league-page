65ac1c2947b83696c999b73f243fd67833137b62
scottiegarcia <82391774+scottiegarcia@users.noreply.github.com>
2022-01-16 14:33:29 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "731530751262183424"; // your league ID
+export const leagueID = "784465525785956352"; // your league ID
 export const leagueName = "Tomcat Dynasty League"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
